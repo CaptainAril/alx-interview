a3b52d6fe5e4db72d689702ecd20b3e7826ed545
CaptainAril <obeemmanuelifeoluwa@gmail.com>
2023-04-14 05:58:25 +0100

    log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -5,12 +5,16 @@ import sys
 
 count = 0
 total_size = 0
+statsCode = {}
 possible_code = [200, 301, 400, 401, 403, 404, 405, 500]
 
 for line in sys.stdin:
     if count == 10:
+        for key, value in sorted(statsCode.items()):
+            print("{}: {}".format(key, value))
         print("File size: {}".format(total_size))
         count = 0
+        statsCode = {}
 
     try:
         count += 1
@@ -21,6 +25,7 @@ for line in sys.stdin:
             code = int(splt[8])
             if code not in possible_code:
                 continue
+            statsCode[code] = count
             print("{}: {}".format(code, count))
 
         except Exception:
