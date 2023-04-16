fa1739dc0e4f7c58230127d2aff62ab93433aee1
CaptainAril <obeemmanuelifeoluwa@gmail.com>
2023-04-16 21:38:31 +0100

    log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,34 +1,48 @@
 #!/usr/bin/python3
 """Reads stdin line by line and computes metrics."""
 
-import sys
 
-count = 0
-total_size = 0
-statsCode = {}
-possible_code = [200, 301, 400, 401, 403, 404, 405, 500]
+def returnLog(total_size, statsCode):
+    """Prints statistics."""
+    print("File size: {}".format(total_size))
+    for key, value in sorted(statsCode.items()):
+        print("{}: {}".format(key, value))
 
-for line in sys.stdin:
-    if count == 10:
-        print("File size: {}".format(total_size))
-        for key, value in sorted(statsCode.items()):
-            print("{}: {}".format(key, value))
-        count = 0
-        statsCode = {}
 
-    try:
-        count += 1
-        splt = line.split(' ')
+def log():
+    """Reads stdin line by line and computes metrics."""
+    import sys
+    count = 0
+    total_size = 0
+    statsCode = {}
+    possible_code = [200, 301, 400, 401, 403, 404, 405, 500]
+    for line in sys.stdin:
+        if count == 10:
+            returnLog(total_size, statsCode)
+            count = 0
+            statsCode = {}
+
         try:
-            code = int(splt[7])
-            size = int(splt[8])
-            total_size += size
-            if code not in possible_code:
-                continue
-            statsCode[code] = count
+            count += 1
+            splt = line.split(' ')
+            try:
+                code = int(splt[7])
+                size = int(splt[8])
+                total_size += size
+                if code not in possible_code:
+                    continue
+                if code in statsCode.keys():
+                    statsCode[code] += 1
+                else:
+                    statsCode[code] = 1
 
-        except Exception:
-            pass
+            except Exception:
+                pass
 
-    except KeyboardInterrupt:
-        sys.exit(0)
+        except KeyboardInterrupt as e:
+            returnLog(total_size, statsCode)
+            raise
+
+
+if __name__ == '__main__':
+    log()
